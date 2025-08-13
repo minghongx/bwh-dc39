cb7c47dd1ca2b2928545ec715dff671419ef3de6
Minghong Xu <minghong@minghongxu.name>
2025-08-14 02:21:28 +0900

    remove virtio_pci and virtio_scsi kernel modules
    They are already listed in
    https://github.com/NixOS/nixpkgs/blob/039f7e165daf359bacfbcea4801453274ab9e4ac/nixos/modules/profiles/qemu-guest.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "xhci_pci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
